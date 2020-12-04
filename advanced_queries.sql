f4d709965051b4e5dabc9ccacef865640f34d2a7
Jessie Chen <yhcjessie1@gmail.com>
2020-12-04 16:34:17 -0500

    sql another way

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,14 +2,39 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+select * 
+from authors a
+join books b on a.id=b.author_id
+where a.id=8
+
+select * 
+from authors a
+join books b on a.id=b.author_id
+where a.name like 'George%'
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+select * 
+from authors a
+join books b on a.id=b.author_id
+where a.name like 'Milan%'
+
+select * 
+from authors a, books b
+where  a.id=b.author_id  and a.name like 'Milan%'
 
 -- Find all books written by an author from China or the UK.
-
+select * 
+from authors a
+join books b on a.id=b.author_id
+where a.nationality = ('China') or a.nationality like ('United K%');
 -- Find out how many books Albert Camus wrote.
-
+select count(b.id) as count, b.title, a.name
+from authors a
+join books b on a.id=b.author_id
+where a.name='Albert Camus'
+group by b.title, a.name;
 -- Find out how many books were written by US authors.
+
 
 -- Find all books written after 1930 by authors from Argentina.
 
