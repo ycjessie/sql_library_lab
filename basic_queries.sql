546a45d815f6f21fa33d89073a80cb8deb43bd04
Jessie Chen <yhcjessie1@gmail.com>
2020-12-04 15:55:11 -0500

    basic sql update

--- basic_queries.sql
+++ basic_queries.sql
@@ -21,12 +21,22 @@ where publication_date<1989;
 select title from books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+select publication_date from books
+where title='A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
+select * from books
+where title ilike '%the%';
 
 -- Add yourself as an author
-
+INSERT INTO authors( name, nationality, birth_year) 
+VALUES ('Jessie Chen', 'Taiwan', 1980);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
+INSERT INTO books( title, publication_date, author_id) 
+VALUES ('Yummy Dumplings','2020', 13);
+INSERT INTO books( title, publication_date, author_id) 
+VALUES ('wow','2021', 13);
+INSERT INTO books( title, publication_date, author_id) 
+VALUES ('Challenges','2020', 13);
 
 -- Update one of your books to have a new title
 
